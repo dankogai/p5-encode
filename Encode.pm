352208ae5ec5986329d3e6f3a6ed20010e9bc71b
dankogai <dankogai@d0d07461-0603-4401-acd4-de1884942a52>
2009-03-07 07:47:30 +0000

    VERSION 2.32
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Encode/trunk@30911 d0d07461-0603-4401-acd4-de1884942a52

--- Encode.pm
+++ Encode.pm
@@ -1,10 +1,10 @@
 #
-# $Id: Encode.pm,v 2.31 2009/02/16 06:13:11 dankogai Exp $
+# $Id: Encode.pm,v 2.32 2009/03/07 07:32:37 dankogai Exp dankogai $
 #
 package Encode;
 use strict;
 use warnings;
-our $VERSION = sprintf "%d.%02d", q$Revision: 2.31 $ =~ /(\d+)/g;
+our $VERSION = sprintf "%d.%02d", q$Revision: 2.32 $ =~ /(\d+)/g;
 sub DEBUG () { 0 }
 use XSLoader ();
 XSLoader::load( __PACKAGE__, $VERSION );
