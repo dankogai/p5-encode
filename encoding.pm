dankogai/p5-encode

commit 5ac059564287c10e630675af3b65f6e8b1103528

VERSION 2.72

encoding.pm
@@ -1,12 +1,19 @@
-# $Id: encoding.pm,v 2.13 2015/03/12 00:03:52 dankogai Exp $
+# $Id: encoding.pm,v 2.14 2015/03/14 02:44:39 dankogai Exp dankogai $
 package encoding;
-our $VERSION = sprintf "%d.%02d", q$Revision: 2.13 $ =~ /(\d+)/g;
+our $VERSION = sprintf "%d.%02d", q$Revision: 2.14 $ =~ /(\d+)/g;
 
 use Encode;
 use strict;
 use warnings;
 
 use constant DEBUG => !!$ENV{PERL_ENCODE_DEBUG};
+
+BEGIN {
+    if ( ord("A") == 193 ) {
+        require Carp;
+        Carp::croak("encoding: pragma does not support EBCDIC platforms");
+    }
+}
 
 our $HAS_PERLIO = 0;
 eval { require PerlIO::encoding };
@@ -95,11 +102,6 @@ sub _get_locale_encoding {
 }
 
 sub import {
-    if ( ord("A") == 193 ) {
-        require Carp;
-        Carp::croak("encoding: pragma does not support EBCDIC platforms");
-    }
-
     if ($] >= 5.017) {
 	warnings::warnif("deprecated",
 			 "Use of the encoding pragma is deprecated")

Encode.pm
@@ -1,10 +1,10 @@
 #
-# $Id: Encode.pm,v 2.71 2015/03/12 00:04:33 dankogai Exp $
+# $Id: Encode.pm,v 2.72 2015/03/14 02:43:24 dankogai Exp $
 #
 package Encode;
 use strict;
 use warnings;
-our $VERSION = sprintf "%d.%02d", q$Revision: 2.71 $ =~ /(\d+)/g;
+our $VERSION = sprintf "%d.%02d", q$Revision: 2.72 $ =~ /(\d+)/g;
 use constant DEBUG => !!$ENV{PERL_ENCODE_DEBUG};
 use XSLoader ();
 XSLoader::load( __PACKAGE__, $VERSION );

commit 77db44078378e49dd2f05538a6d7a9ef5d8cdc57

Allow 'no encoding' on EBCDIC platforms.
Commit 5ac059564287c10e630675af3b65f6e8b1103528 mistakenly reverted
afcba14d20549efcf80323ccf67c4fa36f5c16be.  This commit reinstates it.
The original commit message was:

On an EBCDIC platform, if someone specifies 'no encoding' it croaks at
compile time.  But it isn't a problem to specify 'no encoding'.  It is
only a problem to specify 'use encoding'.  So move the test to the
import function.

encoding.pm
@@ -12,13 +12,6 @@ use constant {
     PERL_5_21_7 => $^V && $^V ge v5.21.7,
 };
 
-BEGIN {
-    if ( ord("A") == 193 ) {
-        require Carp;
-        Carp::croak("encoding: pragma does not support EBCDIC platforms");
-    }
-}
-
 sub _exception {
     my $name = shift;
     $] > 5.008 and return 0;    # 5.8.1 or higher then no
@@ -115,6 +108,12 @@ sub _get_locale_encoding {
 }
 
 sub import {
+
+    if ( ord("A") == 193 ) {
+        require Carp;
+        Carp::croak("encoding: pragma does not support EBCDIC platforms");
+    }
+
     if ($] >= 5.017) {
 	warnings::warnif("deprecated",
 			 "Use of the encoding pragma is deprecated")