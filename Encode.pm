f88692871dce592438c65a8d6ea42b3897f18033
Dan Kogai <dankogai+github@gmail.com>
2020-03-18 13:57:41 +0900

    version 3.05

--- Encode.pm
+++ Encode.pm
@@ -1,5 +1,5 @@
 #
-# $Id: Encode.pm,v 3.04 2020/03/10 22:26:29 dankogai Exp $
+# $Id: Encode.pm,v 3.05 2020/03/18 04:55:37 dankogai Exp $
 #
 package Encode;
 use strict;
@@ -7,7 +7,7 @@ use warnings;
 use constant DEBUG => !!$ENV{PERL_ENCODE_DEBUG};
 our $VERSION;
 BEGIN {
-    $VERSION = sprintf "%d.%02d", q$Revision: 3.04 $ =~ /(\d+)/g;
+    $VERSION = sprintf "%d.%02d", q$Revision: 3.05 $ =~ /(\d+)/g;
     require XSLoader;
     XSLoader::load( __PACKAGE__, $VERSION );
 }
