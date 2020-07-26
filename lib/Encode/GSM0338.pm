04a42d4191381403cce31556584132b91dac1127
Branislav Zahradník <happy.barney@gmail.com>
2020-07-26 11:44:20 +0200

    Split map table into basic and shift set; use GSM->UNI as primary

--- lib/Encode/GSM0338.pm
+++ lib/Encode/GSM0338.pm
@@ -22,154 +22,193 @@ use utf8;
 
 # Mapping table according to 3GPP TS 23.038 version 16.0.0 Release 16 and ETSI TS 123 038 V16.0.0 (2020-07)
 # https://www.etsi.org/deliver/etsi_ts/123000_123099/123038/16.00.00_60/ts_123038v160000p.pdf (page 20 and 22)
-our %UNI2GSM = (
-    "\x{000A}" => "\x0A",        # LINE FEED
-    "\x{000C}" => "\x1B\x0A",    # FORM FEED
-    "\x{000D}" => "\x0D",        # CARRIAGE RETURN
-    "\x{0020}" => "\x20",        # SPACE
-    "\x{0021}" => "\x21",        # EXCLAMATION MARK
-    "\x{0022}" => "\x22",        # QUOTATION MARK
-    "\x{0023}" => "\x23",        # NUMBER SIGN
-    "\x{0024}" => "\x02",        # DOLLAR SIGN
-    "\x{0025}" => "\x25",        # PERCENT SIGN
-    "\x{0026}" => "\x26",        # AMPERSAND
-    "\x{0027}" => "\x27",        # APOSTROPHE
-    "\x{0028}" => "\x28",        # LEFT PARENTHESIS
-    "\x{0029}" => "\x29",        # RIGHT PARENTHESIS
-    "\x{002A}" => "\x2A",        # ASTERISK
-    "\x{002B}" => "\x2B",        # PLUS SIGN
-    "\x{002C}" => "\x2C",        # COMMA
-    "\x{002D}" => "\x2D",        # HYPHEN-MINUS
-    "\x{002E}" => "\x2E",        # FULL STOP
-    "\x{002F}" => "\x2F",        # SOLIDUS
-    "\x{0030}" => "\x30",        # DIGIT ZERO
-    "\x{0031}" => "\x31",        # DIGIT ONE
-    "\x{0032}" => "\x32",        # DIGIT TWO
-    "\x{0033}" => "\x33",        # DIGIT THREE
-    "\x{0034}" => "\x34",        # DIGIT FOUR
-    "\x{0035}" => "\x35",        # DIGIT FIVE
-    "\x{0036}" => "\x36",        # DIGIT SIX
-    "\x{0037}" => "\x37",        # DIGIT SEVEN
-    "\x{0038}" => "\x38",        # DIGIT EIGHT
-    "\x{0039}" => "\x39",        # DIGIT NINE
-    "\x{003A}" => "\x3A",        # COLON
-    "\x{003B}" => "\x3B",        # SEMICOLON
-    "\x{003C}" => "\x3C",        # LESS-THAN SIGN
-    "\x{003D}" => "\x3D",        # EQUALS SIGN
-    "\x{003E}" => "\x3E",        # GREATER-THAN SIGN
-    "\x{003F}" => "\x3F",        # QUESTION MARK
-    "\x{0040}" => "\x00",        # COMMERCIAL AT
-    "\x{0041}" => "\x41",        # LATIN CAPITAL LETTER A
-    "\x{0042}" => "\x42",        # LATIN CAPITAL LETTER B
-    "\x{0043}" => "\x43",        # LATIN CAPITAL LETTER C
-    "\x{0044}" => "\x44",        # LATIN CAPITAL LETTER D
-    "\x{0045}" => "\x45",        # LATIN CAPITAL LETTER E
-    "\x{0046}" => "\x46",        # LATIN CAPITAL LETTER F
-    "\x{0047}" => "\x47",        # LATIN CAPITAL LETTER G
-    "\x{0048}" => "\x48",        # LATIN CAPITAL LETTER H
-    "\x{0049}" => "\x49",        # LATIN CAPITAL LETTER I
-    "\x{004A}" => "\x4A",        # LATIN CAPITAL LETTER J
-    "\x{004B}" => "\x4B",        # LATIN CAPITAL LETTER K
-    "\x{004C}" => "\x4C",        # LATIN CAPITAL LETTER L
-    "\x{004D}" => "\x4D",        # LATIN CAPITAL LETTER M
-    "\x{004E}" => "\x4E",        # LATIN CAPITAL LETTER N
-    "\x{004F}" => "\x4F",        # LATIN CAPITAL LETTER O
-    "\x{0050}" => "\x50",        # LATIN CAPITAL LETTER P
-    "\x{0051}" => "\x51",        # LATIN CAPITAL LETTER Q
-    "\x{0052}" => "\x52",        # LATIN CAPITAL LETTER R
-    "\x{0053}" => "\x53",        # LATIN CAPITAL LETTER S
-    "\x{0054}" => "\x54",        # LATIN CAPITAL LETTER T
-    "\x{0055}" => "\x55",        # LATIN CAPITAL LETTER U
-    "\x{0056}" => "\x56",        # LATIN CAPITAL LETTER V
-    "\x{0057}" => "\x57",        # LATIN CAPITAL LETTER W
-    "\x{0058}" => "\x58",        # LATIN CAPITAL LETTER X
-    "\x{0059}" => "\x59",        # LATIN CAPITAL LETTER Y
-    "\x{005A}" => "\x5A",        # LATIN CAPITAL LETTER Z
-    "\x{005B}" => "\x1B\x3C",    # LEFT SQUARE BRACKET
-    "\x{005C}" => "\x1B\x2F",    # REVERSE SOLIDUS
-    "\x{005D}" => "\x1B\x3E",    # RIGHT SQUARE BRACKET
-    "\x{005E}" => "\x1B\x14",    # CIRCUMFLEX ACCENT
-    "\x{005F}" => "\x11",        # LOW LINE
-    "\x{0061}" => "\x61",        # LATIN SMALL LETTER A
-    "\x{0062}" => "\x62",        # LATIN SMALL LETTER B
-    "\x{0063}" => "\x63",        # LATIN SMALL LETTER C
-    "\x{0064}" => "\x64",        # LATIN SMALL LETTER D
-    "\x{0065}" => "\x65",        # LATIN SMALL LETTER E
-    "\x{0066}" => "\x66",        # LATIN SMALL LETTER F
-    "\x{0067}" => "\x67",        # LATIN SMALL LETTER G
-    "\x{0068}" => "\x68",        # LATIN SMALL LETTER H
-    "\x{0069}" => "\x69",        # LATIN SMALL LETTER I
-    "\x{006A}" => "\x6A",        # LATIN SMALL LETTER J
-    "\x{006B}" => "\x6B",        # LATIN SMALL LETTER K
-    "\x{006C}" => "\x6C",        # LATIN SMALL LETTER L
-    "\x{006D}" => "\x6D",        # LATIN SMALL LETTER M
-    "\x{006E}" => "\x6E",        # LATIN SMALL LETTER N
-    "\x{006F}" => "\x6F",        # LATIN SMALL LETTER O
-    "\x{0070}" => "\x70",        # LATIN SMALL LETTER P
-    "\x{0071}" => "\x71",        # LATIN SMALL LETTER Q
-    "\x{0072}" => "\x72",        # LATIN SMALL LETTER R
-    "\x{0073}" => "\x73",        # LATIN SMALL LETTER S
-    "\x{0074}" => "\x74",        # LATIN SMALL LETTER T
-    "\x{0075}" => "\x75",        # LATIN SMALL LETTER U
-    "\x{0076}" => "\x76",        # LATIN SMALL LETTER V
-    "\x{0077}" => "\x77",        # LATIN SMALL LETTER W
-    "\x{0078}" => "\x78",        # LATIN SMALL LETTER X
-    "\x{0079}" => "\x79",        # LATIN SMALL LETTER Y
-    "\x{007A}" => "\x7A",        # LATIN SMALL LETTER Z
-    "\x{007B}" => "\x1B\x28",    # LEFT CURLY BRACKET
-    "\x{007C}" => "\x1B\x40",    # VERTICAL LINE
-    "\x{007D}" => "\x1B\x29",    # RIGHT CURLY BRACKET
-    "\x{007E}" => "\x1B\x3D",    # TILDE
-    "\x{00A1}" => "\x40",        # INVERTED EXCLAMATION MARK
-    "\x{00A3}" => "\x01",        # POUND SIGN
-    "\x{00A4}" => "\x24",        # CURRENCY SIGN
-    "\x{00A5}" => "\x03",        # YEN SIGN
-    "\x{00A7}" => "\x5F",        # SECTION SIGN
-    "\x{00BF}" => "\x60",        # INVERTED QUESTION MARK
-    "\x{00C4}" => "\x5B",        # LATIN CAPITAL LETTER A WITH DIAERESIS
-    "\x{00C5}" => "\x0E",        # LATIN CAPITAL LETTER A WITH RING ABOVE
-    "\x{00C6}" => "\x1C",        # LATIN CAPITAL LETTER AE
-    "\x{00C7}" => "\x09",        # LATIN CAPITAL LETTER C WITH CEDILLA
-    "\x{00C9}" => "\x1F",        # LATIN CAPITAL LETTER E WITH ACUTE
-    "\x{00D1}" => "\x5D",        # LATIN CAPITAL LETTER N WITH TILDE
-    "\x{00D6}" => "\x5C",        # LATIN CAPITAL LETTER O WITH DIAERESIS
-    "\x{00D8}" => "\x0B",        # LATIN CAPITAL LETTER O WITH STROKE
-    "\x{00DC}" => "\x5E",        # LATIN CAPITAL LETTER U WITH DIAERESIS
-    "\x{00DF}" => "\x1E",        # LATIN SMALL LETTER SHARP S
-    "\x{00E0}" => "\x7F",        # LATIN SMALL LETTER A WITH GRAVE
-    "\x{00E4}" => "\x7B",        # LATIN SMALL LETTER A WITH DIAERESIS
-    "\x{00E5}" => "\x0F",        # LATIN SMALL LETTER A WITH RING ABOVE
-    "\x{00E6}" => "\x1D",        # LATIN SMALL LETTER AE
-    "\x{00E8}" => "\x04",        # LATIN SMALL LETTER E WITH GRAVE
-    "\x{00E9}" => "\x05",        # LATIN SMALL LETTER E WITH ACUTE
-    "\x{00EC}" => "\x07",        # LATIN SMALL LETTER I WITH GRAVE
-    "\x{00F1}" => "\x7D",        # LATIN SMALL LETTER N WITH TILDE
-    "\x{00F2}" => "\x08",        # LATIN SMALL LETTER O WITH GRAVE
-    "\x{00F6}" => "\x7C",        # LATIN SMALL LETTER O WITH DIAERESIS
-    "\x{00F8}" => "\x0C",        # LATIN SMALL LETTER O WITH STROKE
-    "\x{00F9}" => "\x06",        # LATIN SMALL LETTER U WITH GRAVE
-    "\x{00FC}" => "\x7E",        # LATIN SMALL LETTER U WITH DIAERESIS
-    "\x{0393}" => "\x13",        # GREEK CAPITAL LETTER GAMMA
-    "\x{0394}" => "\x10",        # GREEK CAPITAL LETTER DELTA
-    "\x{0398}" => "\x19",        # GREEK CAPITAL LETTER THETA
-    "\x{039B}" => "\x14",        # GREEK CAPITAL LETTER LAMDA
-    "\x{039E}" => "\x1A",        # GREEK CAPITAL LETTER XI
-    "\x{03A0}" => "\x16",        # GREEK CAPITAL LETTER PI
-    "\x{03A3}" => "\x18",        # GREEK CAPITAL LETTER SIGMA
-    "\x{03A6}" => "\x12",        # GREEK CAPITAL LETTER PHI
-    "\x{03A8}" => "\x17",        # GREEK CAPITAL LETTER PSI
-    "\x{03A9}" => "\x15",        # GREEK CAPITAL LETTER OMEGA
-    "\x{20AC}" => "\x1B\x65",    # EURO SIGN
+our %GSM2UNI_BASIC_SET_PLAIN = (
+    "\x00" => "\x{0040}",        # COMMERCIAL AT
+    "\x01" => "\x{00A3}",        # POUND SIGN
+    "\x02" => "\x{0024}",        # DOLLAR SIGN
+    "\x03" => "\x{00A5}",        # YEN SIGN
+    "\x04" => "\x{00E8}",        # LATIN SMALL LETTER E WITH GRAVE
+    "\x05" => "\x{00E9}",        # LATIN SMALL LETTER E WITH ACUTE
+    "\x06" => "\x{00F9}",        # LATIN SMALL LETTER U WITH GRAVE
+    "\x07" => "\x{00EC}",        # LATIN SMALL LETTER I WITH GRAVE
+    "\x08" => "\x{00F2}",        # LATIN SMALL LETTER O WITH GRAVE
+    "\x09" => "\x{00C7}",        # LATIN CAPITAL LETTER C WITH CEDILLA
+    "\x0A" => "\x{000A}",        # LINE FEED
+    "\x0B" => "\x{00D8}",        # LATIN CAPITAL LETTER O WITH STROKE
+    "\x0C" => "\x{00F8}",        # LATIN SMALL LETTER O WITH STROKE
+    "\x0D" => "\x{000D}",        # CARRIAGE RETURN
+    "\x0E" => "\x{00C5}",        # LATIN CAPITAL LETTER A WITH RING ABOVE
+    "\x0F" => "\x{00E5}",        # LATIN SMALL LETTER A WITH RING ABOVE
+    "\x10" => "\x{0394}",        # GREEK CAPITAL LETTER DELTA
+    "\x11" => "\x{005F}",        # LOW LINE
+    "\x12" => "\x{03A6}",        # GREEK CAPITAL LETTER PHI
+    "\x13" => "\x{0393}",        # GREEK CAPITAL LETTER GAMMA
+    "\x14" => "\x{039B}",        # GREEK CAPITAL LETTER LAMDA
+    "\x15" => "\x{03A9}",        # GREEK CAPITAL LETTER OMEGA
+    "\x16" => "\x{03A0}",        # GREEK CAPITAL LETTER PI
+    "\x17" => "\x{03A8}",        # GREEK CAPITAL LETTER PSI
+    "\x18" => "\x{03A3}",        # GREEK CAPITAL LETTER SIGMA
+    "\x19" => "\x{0398}",        # GREEK CAPITAL LETTER THETA
+    "\x1A" => "\x{039E}",        # GREEK CAPITAL LETTER XI
+    "\x1C" => "\x{00C6}",        # LATIN CAPITAL LETTER AE
+    "\x1D" => "\x{00E6}",        # LATIN SMALL LETTER AE
+    "\x1E" => "\x{00DF}",        # LATIN SMALL LETTER SHARP S
+    "\x1F" => "\x{00C9}",        # LATIN CAPITAL LETTER E WITH ACUTE
+    "\x20" => "\x{0020}",        # SPACE
+    "\x21" => "\x{0021}",        # EXCLAMATION MARK
+    "\x22" => "\x{0022}",        # QUOTATION MARK
+    "\x23" => "\x{0023}",        # NUMBER SIGN
+    "\x24" => "\x{00A4}",        # CURRENCY SIGN
+    "\x25" => "\x{0025}",        # PERCENT SIGN
+    "\x26" => "\x{0026}",        # AMPERSAND
+    "\x27" => "\x{0027}",        # APOSTROPHE
+    "\x28" => "\x{0028}",        # LEFT PARENTHESIS
+    "\x29" => "\x{0029}",        # RIGHT PARENTHESIS
+    "\x2A" => "\x{002A}",        # ASTERISK
+    "\x2B" => "\x{002B}",        # PLUS SIGN
+    "\x2C" => "\x{002C}",        # COMMA
+    "\x2D" => "\x{002D}",        # HYPHEN-MINUS
+    "\x2E" => "\x{002E}",        # FULL STOP
+    "\x2F" => "\x{002F}",        # SOLIDUS
+    "\x30" => "\x{0030}",        # DIGIT ZERO
+    "\x31" => "\x{0031}",        # DIGIT ONE
+    "\x32" => "\x{0032}",        # DIGIT TWO
+    "\x33" => "\x{0033}",        # DIGIT THREE
+    "\x34" => "\x{0034}",        # DIGIT FOUR
+    "\x35" => "\x{0035}",        # DIGIT FIVE
+    "\x36" => "\x{0036}",        # DIGIT SIX
+    "\x37" => "\x{0037}",        # DIGIT SEVEN
+    "\x38" => "\x{0038}",        # DIGIT EIGHT
+    "\x39" => "\x{0039}",        # DIGIT NINE
+    "\x3A" => "\x{003A}",        # COLON
+    "\x3B" => "\x{003B}",        # SEMICOLON
+    "\x3C" => "\x{003C}",        # LESS-THAN SIGN
+    "\x3D" => "\x{003D}",        # EQUALS SIGN
+    "\x3E" => "\x{003E}",        # GREATER-THAN SIGN
+    "\x3F" => "\x{003F}",        # QUESTION MARK
+    "\x40" => "\x{00A1}",        # INVERTED EXCLAMATION MARK
+    "\x41" => "\x{0041}",        # LATIN CAPITAL LETTER A
+    "\x42" => "\x{0042}",        # LATIN CAPITAL LETTER B
+    "\x43" => "\x{0043}",        # LATIN CAPITAL LETTER C
+    "\x44" => "\x{0044}",        # LATIN CAPITAL LETTER D
+    "\x45" => "\x{0045}",        # LATIN CAPITAL LETTER E
+    "\x46" => "\x{0046}",        # LATIN CAPITAL LETTER F
+    "\x47" => "\x{0047}",        # LATIN CAPITAL LETTER G
+    "\x48" => "\x{0048}",        # LATIN CAPITAL LETTER H
+    "\x49" => "\x{0049}",        # LATIN CAPITAL LETTER I
+    "\x4A" => "\x{004A}",        # LATIN CAPITAL LETTER J
+    "\x4B" => "\x{004B}",        # LATIN CAPITAL LETTER K
+    "\x4C" => "\x{004C}",        # LATIN CAPITAL LETTER L
+    "\x4D" => "\x{004D}",        # LATIN CAPITAL LETTER M
+    "\x4E" => "\x{004E}",        # LATIN CAPITAL LETTER N
+    "\x4F" => "\x{004F}",        # LATIN CAPITAL LETTER O
+    "\x50" => "\x{0050}",        # LATIN CAPITAL LETTER P
+    "\x51" => "\x{0051}",        # LATIN CAPITAL LETTER Q
+    "\x52" => "\x{0052}",        # LATIN CAPITAL LETTER R
+    "\x53" => "\x{0053}",        # LATIN CAPITAL LETTER S
+    "\x54" => "\x{0054}",        # LATIN CAPITAL LETTER T
+    "\x55" => "\x{0055}",        # LATIN CAPITAL LETTER U
+    "\x56" => "\x{0056}",        # LATIN CAPITAL LETTER V
+    "\x57" => "\x{0057}",        # LATIN CAPITAL LETTER W
+    "\x58" => "\x{0058}",        # LATIN CAPITAL LETTER X
+    "\x59" => "\x{0059}",        # LATIN CAPITAL LETTER Y
+    "\x5A" => "\x{005A}",        # LATIN CAPITAL LETTER Z
+    "\x5B" => "\x{00C4}",        # LATIN CAPITAL LETTER A WITH DIAERESIS
+    "\x5C" => "\x{00D6}",        # LATIN CAPITAL LETTER O WITH DIAERESIS
+    "\x5D" => "\x{00D1}",        # LATIN CAPITAL LETTER N WITH TILDE
+    "\x5E" => "\x{00DC}",        # LATIN CAPITAL LETTER U WITH DIAERESIS
+    "\x5F" => "\x{00A7}",        # SECTION SIGN
+    "\x60" => "\x{00BF}",        # INVERTED QUESTION MARK
+    "\x61" => "\x{0061}",        # LATIN SMALL LETTER A
+    "\x62" => "\x{0062}",        # LATIN SMALL LETTER B
+    "\x63" => "\x{0063}",        # LATIN SMALL LETTER C
+    "\x64" => "\x{0064}",        # LATIN SMALL LETTER D
+    "\x65" => "\x{0065}",        # LATIN SMALL LETTER E
+    "\x66" => "\x{0066}",        # LATIN SMALL LETTER F
+    "\x67" => "\x{0067}",        # LATIN SMALL LETTER G
+    "\x68" => "\x{0068}",        # LATIN SMALL LETTER H
+    "\x69" => "\x{0069}",        # LATIN SMALL LETTER I
+    "\x6A" => "\x{006A}",        # LATIN SMALL LETTER J
+    "\x6B" => "\x{006B}",        # LATIN SMALL LETTER K
+    "\x6C" => "\x{006C}",        # LATIN SMALL LETTER L
+    "\x6D" => "\x{006D}",        # LATIN SMALL LETTER M
+    "\x6E" => "\x{006E}",        # LATIN SMALL LETTER N
+    "\x6F" => "\x{006F}",        # LATIN SMALL LETTER O
+    "\x70" => "\x{0070}",        # LATIN SMALL LETTER P
+    "\x71" => "\x{0071}",        # LATIN SMALL LETTER Q
+    "\x72" => "\x{0072}",        # LATIN SMALL LETTER R
+    "\x73" => "\x{0073}",        # LATIN SMALL LETTER S
+    "\x74" => "\x{0074}",        # LATIN SMALL LETTER T
+    "\x75" => "\x{0075}",        # LATIN SMALL LETTER U
+    "\x76" => "\x{0076}",        # LATIN SMALL LETTER V
+    "\x77" => "\x{0077}",        # LATIN SMALL LETTER W
+    "\x78" => "\x{0078}",        # LATIN SMALL LETTER X
+    "\x79" => "\x{0079}",        # LATIN SMALL LETTER Y
+    "\x7A" => "\x{007A}",        # LATIN SMALL LETTER Z
+    "\x7B" => "\x{00E4}",        # LATIN SMALL LETTER A WITH DIAERESIS
+    "\x7C" => "\x{00F6}",        # LATIN SMALL LETTER O WITH DIAERESIS
+    "\x7D" => "\x{00F1}",        # LATIN SMALL LETTER N WITH TILDE
+    "\x7E" => "\x{00FC}",        # LATIN SMALL LETTER U WITH DIAERESIS
+    "\x7F" => "\x{00E0}",        # LATIN SMALL LETTER A WITH GRAVE
 );
-our %GSM2UNI = reverse %UNI2GSM;
+
+our %GSM2UNI_BASIC_SET = (
+	plain => \%GSM2UNI_BASIC_SET_PLAIN,
+);
+
+our %GSM2UNI_SHIFT_SET = (
+	plain => {
+		"\x1B\x0A" => "\x{000C}",    # FORM FEED
+		"\x1B\x14" => "\x{005E}",    # CIRCUMFLEX ACCENT
+		"\x1B\x28" => "\x{007B}",    # LEFT CURLY BRACKET
+		"\x1B\x29" => "\x{007D}",    # RIGHT CURLY BRACKET
+		"\x1B\x2F" => "\x{005C}",    # REVERSE SOLIDUS
+		"\x1B\x3C" => "\x{005B}",    # LEFT SQUARE BRACKET
+		"\x1B\x3D" => "\x{007E}",    # TILDE
+		"\x1B\x3E" => "\x{005D}",    # RIGHT SQUARE BRACKET
+		"\x1B\x40" => "\x{007C}",    # VERTICAL LINE
+		"\x1B\x65" => "\x{20AC}",    # EURO SIGN
+	},
+);
+
+our %UNI2GSM_BASIC_SET = (
+	plain => { reverse %{ $GSM2UNI_BASIC_SET{plain} } },
+);
+
+our %UNI2GSM_SHIFT_SET = (
+	plain => { reverse %{ $GSM2UNI_SHIFT_SET{plain} } },
+);
+
 our $ESC    = "\x1b";
 
+sub basic_set {
+	$_[0]->{basic_set} ||= 'plain';
+}
+
+sub shift_set {
+	$_[0]->{shift_set} ||= 'plain';
+}
+
 sub gsm2uni {
-	return \%GSM2UNI;
+	my ($self) = @_;
+
+	$self->{gsm2uni} ||= do {
+		my $basic_set = $GSM2UNI_BASIC_SET{ $self->basic_set };
+		my $shift_set = $GSM2UNI_SHIFT_SET{ $self->shift_set };
+
+		+{ %$basic_set, %$shift_set };
+	};
 }
 
 sub uni2gsm {
-	return \%UNI2GSM;
+	my ($self) = @_;
+
+	$self->{uni2gsm} ||= do {
+		my $basic_set = $UNI2GSM_BASIC_SET{ $self->basic_set };
+		my $shift_set = $UNI2GSM_SHIFT_SET{ $self->shift_set };
+
+		+{ %$basic_set, %$shift_set };
+	};
 }
 
 sub decode ($$;$) {
